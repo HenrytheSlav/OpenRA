8777195c8966990d94b4661a800580e5580b17b6
HenrytheSlav <steametc@centrum.cz>
2015-06-13 17:17:01 +0200

    Added soviet05

---
+++ mods/ra/maps/soviet-05/AI.lua
@@ -0,0 +1,260 @@
+BuildBase = function()
+	local clock = Map.ActorsInCircle(Map.CenterOfCell(MCVDeploy.Location), WRange.New(512))[1]
+	
+	if not CheckForCYard then return 
+	
+	--Build a Power Plant
+	elseif not b1 then
+	clock.Wait(Actor.BuildTime("powr"))
+	clock.CallFunc(function() powr1 = Actor.Create("powr", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(3,-2) }) b1 = true GoodGuy.Cash = GoodGuy.Cash - 300 Trigger.OnKilledOrCaptured(powr1, function() b1 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("powr") + 25, BuildBase)
+
+	--Build a Barracks then Pillbox
+	elseif b1 and not b2 then 
+	clock.Wait(Actor.BuildTime("tent"))
+	clock.CallFunc(function() tent = Actor.Create("tent", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(0,4) }) b2 = true GoodGuy.Cash = GoodGuy.Cash - 400 Trigger.OnKilledOrCaptured(tent, function() b2 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("tent") + 25, BuildBase)
+	
+	elseif b1 and b2 and not b3 then 
+	clock.Wait(Actor.BuildTime("hbox"))
+	clock.CallFunc(function() hbox1 = Actor.Create("hbox", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(3,6) }) b3 = true GoodGuy.Cash = GoodGuy.Cash - 600 Trigger.OnKilledOrCaptured(hbox1, function() b3 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("hbox") + 25, BuildBase)
+	
+	-- Build a Refinery
+	elseif b1 and b2 and not b4 then
+	clock.Wait(Actor.BuildTime("proc"))
+	clock.CallFunc(function() proc1 = Actor.Create("proc", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(4,2) }) b4 = true GoodGuy.Cash = GoodGuy.Cash - 1400 Trigger.OnKilledOrCaptured(proc1, function() b4 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("proc") + 25, BuildBase)
+	
+	-- Build another Power Plant
+	elseif b1 and b2 and b3 and b4 and not b5 then
+	clock.Wait(Actor.BuildTime("powr"))
+	clock.CallFunc(function() powr2 = Actor.Create("powr", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(5,-3) }) b5 = true GoodGuy.Cash = GoodGuy.Cash - 300 Trigger.OnKilledOrCaptured(powr2, function() b5 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("powr") + 25, BuildBase)
+	
+	-- Build a War Factory
+	elseif b2 and b4 and b5 and not b6 then
+	clock.Wait(Actor.BuildTime("weap"))
+	clock.CallFunc(function() weap1 = Actor.Create("weap", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(-5,3) }) b6 = true GoodGuy.Cash = GoodGuy.Cash - 2000 Trigger.OnKilledOrCaptured(weap1, function() b6 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("weap") + 25, BuildBase)
+		
+	-- Some defences
+	elseif b1 and b2 and b6 and not b7 then
+	clock.Wait(Actor.BuildTime("hbox"))
+	clock.CallFunc(function() hbox2 = Actor.Create("hbox", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(-6,5) }) b7 = true GoodGuy.Cash = GoodGuy.Cash - 600 Trigger.OnKilledOrCaptured(hbox2, function() b7 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("hbox") + 25, BuildBase)
+	
+	elseif b1 and b2 and b7 and not b8 then
+	clock.Wait(Actor.BuildTime("gun"))
+	clock.CallFunc(function() gun1 = Actor.Create("gun", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(0,8) }) b8 = true GoodGuy.Cash = GoodGuy.Cash - 600 Trigger.OnKilledOrCaptured(gun1, function() b8 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("gun") + 25, BuildBase)
+	
+	elseif b1 and b2 and b8 and not b9 then
+	clock.Wait(Actor.BuildTime("gun"))
+	clock.CallFunc(function() gun2 = Actor.Create("gun", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(-4,7) }) b9 = true GoodGuy.Cash = GoodGuy.Cash - 600 Trigger.OnKilledOrCaptured(gun2, function() b9 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("gun") + 25, BuildBase)
+	
+	-- Build another Power Plant
+	elseif b1 and b5 and b6 and b4 and b9 and not b10 then
+	clock.Wait(Actor.BuildTime("powr"))
+	clock.CallFunc(function() powr3 = Actor.Create("powr", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(-4,-3) }) b10 = true GoodGuy.Cash = GoodGuy.Cash - 300 Trigger.OnKilledOrCaptured(powr3, function() b10 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("powr") + 25, BuildBase)
+	
+	-- Build another Refinery
+	elseif b4 and b6 and b5 and b1 and b10 and not b11 then
+	clock.Wait(Actor.BuildTime("proc"))
+	clock.CallFunc(function() proc2 = Actor.Create("proc", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(-9,1) }) b11 = true GoodGuy.Cash = GoodGuy.Cash - 1400 Trigger.OnKilledOrCaptured(proc2, function() b11 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("proc") + 25, BuildBase)
+	
+	-- Build another Power Plant and a silo
+	elseif b1 and b5 and b6 and b4 and b11 and not b12 then
+	clock.Wait(Actor.BuildTime("powr"))
+	clock.CallFunc(function() powr4 = Actor.Create("powr", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(-8,-2) }) b12 = true GoodGuy.Cash = GoodGuy.Cash - 300 Trigger.OnKilledOrCaptured(powr4, function() b12 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("powr") + 25, BuildBase)
+	
+	elseif b1 and b5 and b11 and b4 and b12 and not b13 then
+	clock.Wait(Actor.BuildTime("silo"))
+	clock.CallFunc(function() silo = Actor.Create("silo", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(6,0) }) b13 = true GoodGuy.Cash = GoodGuy.Cash - 150 Trigger.OnKilledOrCaptured(silo, function() b13 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("silo") + 25, BuildBase)
+	
+	-- Some defences
+	elseif b1 and b2 and b13 and not b14 then
+	clock.Wait(Actor.BuildTime("agun"))
+	clock.CallFunc(function() aa1 = Actor.Create("agun", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(-3,0) }) b14 = true GoodGuy.Cash = GoodGuy.Cash - 800 Trigger.OnKilledOrCaptured(aa1, function() b14 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("agun") + 25, BuildBase)
+	
+	-- Build another Power Plant
+	elseif b1 and b5 and b6 and b4 and b10 and b14 and not b15 then
+	clock.Wait(Actor.BuildTime("powr"))
+	clock.CallFunc(function() powr5 = Actor.Create("powr", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(-6,-2) }) b15 = true GoodGuy.Cash = GoodGuy.Cash - 300 Trigger.OnKilledOrCaptured(powr5, function() b15 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("powr") + 25, BuildBase)
+	
+	-- Some defences
+	elseif b1 and b2 and b15 and not b16 then
+	clock.Wait(Actor.BuildTime("agun"))
+	clock.CallFunc(function() aa2 = Actor.Create("agun", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(4,1) }) b16 = true GoodGuy.Cash = GoodGuy.Cash - 800 Trigger.OnKilledOrCaptured(aa2, function() b16 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("agun") + 25, BuildBase)
+	
+	elseif b1 and b2 and b16 and not b17 then
+	clock.Wait(Actor.BuildTime("gun"))
+	clock.CallFunc(function() gun3 = Actor.Create("gun", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(-9,5) }) b17 = true GoodGuy.Cash = GoodGuy.Cash - 600 Trigger.OnKilledOrCaptured(gun3, function() b17 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("gun") + 25, BuildBase)
+	
+	elseif b1 and b2 and b17 and not b18 then
+	clock.Wait(Actor.BuildTime("gun"))
+	clock.CallFunc(function() gun4 = Actor.Create("gun", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(-2,-3) }) b18 = true GoodGuy.Cash = GoodGuy.Cash - 600 Trigger.OnKilledOrCaptured(gun4, function() b18 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("gun") + 25, BuildBase)
+		
+	-- Build another Power Plant
+	elseif b1 and b5 and b6 and b4 and b10 and b14 and b18 and not b22 then
+	clock.Wait(Actor.BuildTime("powr"))
+	clock.CallFunc(function() powr6 = Actor.Create("powr", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(4,-6) }) b22 = true GoodGuy.Cash = GoodGuy.Cash - 300 Trigger.OnKilledOrCaptured(powr6, function() b22 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("powr") + 25, BuildBase)
+	
+	elseif b1 and b2 and b18 and b22 and not b19 then
+	clock.Wait(Actor.BuildTime("gun"))
+	clock.CallFunc(function() gun5 = Actor.Create("gun", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(3,-6) }) b19 = true GoodGuy.Cash = GoodGuy.Cash - 600 Trigger.OnKilledOrCaptured(gun5, function() b19 = false end)  end)
+	Trigger.AfterDelay(Actor.BuildTime("gun") + 25, BuildBase)
+	
+	elseif b1 and b2 and b19 and not b20 then
+	clock.Wait(Actor.BuildTime("hbox"))
+	clock.CallFunc(function() hbox3 = Actor.Create("hbox", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(3,-4) }) b20 = true GoodGuy.Cash = GoodGuy.Cash - 600 Trigger.OnKilledOrCaptured(hbox3, function() b20 = false end) end)
+	Trigger.AfterDelay(Actor.BuildTime("hbox") + 25, BuildBase)
+	
+	elseif b1 and b2 and b20 and not b21 then
+	clock.Wait(Actor.BuildTime("gun"))
+	clock.CallFunc(function() gun6 = Actor.Create("gun", true, { Owner = GoodGuy, Location = MCVDeploy.Location + CVec.New(2,3) }) b20 = true GoodGuy.Cash = GoodGuy.Cash - 600 Trigger.OnKilledOrCaptured(gun6, function() b20 = false end)  end)
+	Trigger.AfterDelay(Actor.BuildTime("gun") + 25, BuildBase)
+	
+	else
+	Trigger.AfterDelay(DateTime.Seconds(5), BuildBase)
+	end
+	-- Media.DisplayMessage("Debug: Build Chain Triggered")
+end
+
+ProduceInfantry = function()
+	if Barr.IsDead then
+		return
+	end
+	
+	if StopInfantry == true then
+		Trigger.AfterDelay(DateTime.Minutes(2), ProduceInfantry)
+		StopInfantry = false
+		GreeceInfAttack = {}
+		return
+	end
+	
+	local delay = Utils.RandomInteger(DateTime.Seconds(3), DateTime.Seconds(9))
+	local toBuild = { Utils.Random(AlliedInfantryTypes) }
+	Greece.Build(toBuild, function(unit)
+		GreeceInfAttack[#GreeceInfAttack + 1] = unit[1]
+		Trigger.AfterDelay(delay, ProduceInfantry)
+
+		if #GreeceInfAttack >= 7 then
+			SendInfantry()
+			StopInfantry = true
+		end
+	end)
+end
+
+ProduceShips = function()
+	if Shipyard.IsDead then
+		return
+	end
+	
+	if StopShips == true then
+		Trigger.AfterDelay(DateTime.Minutes(6), ProduceShips)
+		StopShips = false
+		Ships = {}
+		return
+	end
+
+	Greece.Build( {"dd"}, function(unit)
+		Ships[#Ships + 1] = unit[1]
+		Trigger.AfterDelay(Actor.BuildTime( "dd" ), ProduceShips)
+
+		if #Ships >= 2 then
+			SendShips()
+			StopShips = true
+		end
+	end)
+end
+
+SendShips = function()
+	Utils.Do(Ships, function(ship) ship.AttackMove(waypoint77.Location) end)
+	Utils.Do(Ships, function(ship) ship.AttackMove(waypoint14.Location) end)
+	Utils.Do(Ships, function(ship) ship.AttackMove(waypoint51.Location) end)
+	Utils.Do(Ships, function(ship) ship.Hunt() end)
+end
+
+SendInfantry = function()
+	Utils.Do(GreeceInfAttack, function(inf) inf.AttackMove(waypoint2.Location) end)
+	Utils.Do(GreeceInfAttack, function(inf) inf.AttackMove(waypoint48.Location) end)
+	Utils.Do(GreeceInfAttack, function(inf) inf.AttackMove(waypoint49.Location) end)
+	Utils.Do(GreeceInfAttack, function(inf) inf.AttackMove(waypoint1.Location) end)
+	Utils.Do(GreeceInfAttack, function(inf) inf.Hunt() end)
+end
+
+ProduceInfantryGG = function()
+	if not b2 then
+		return
+	end
+	
+	if StopInfantryGG == true then
+		Trigger.AfterDelay(DateTime.Minutes(2), ProduceInfantryGG)
+		StopInfantryGG = false
+		GGInfAttack = {}
+		return
+	end
+	
+	local delay = Utils.RandomInteger(DateTime.Seconds(3), DateTime.Seconds(9))
+	local toBuild = { Utils.Random(AlliedInfantryTypes) }
+	GoodGuy.Build(toBuild, function(unit)
+		GGInfAttack[#GGInfAttack + 1] = unit[1]
+		Trigger.AfterDelay(delay, ProduceInfantryGG)
+
+		if #GGInfAttack >= 10 then
+			SendInfantryGG()
+			StopInfantryGG = true
+		end
+	end)
+end
+
+SendInfantryGG = function()
+	Utils.Do(GGInfAttack, function(inf) inf.AttackMove(waypoint29.Location) end)
+	Utils.Do(GGInfAttack, function(inf) inf.AttackMove(waypoint30.Location) end)
+	Utils.Do(GGInfAttack, function(inf) inf.AttackMove(waypoint40.Location) end)
+	Utils.Do(GGInfAttack, function(inf) inf.Hunt() end)
+end
+
+ProduceTanksGG = function()
+	if not b6 then
+		return
+	end
+	
+	if StopTanksGG == true then
+		Trigger.AfterDelay(DateTime.Minutes(3), ProduceTanksGG)
+		StopTanksGG = false
+		TankAttackGG = {}
+		return
+	end
+	
+	local delay = Utils.RandomInteger(DateTime.Seconds(12), DateTime.Seconds(17))
+	local toBuild = { Utils.Random(AlliedTankTypes) }
+	GoodGuy.Build(toBuild, function(unit)
+		TankAttackGG[#TankAttackGG + 1] = unit[1]
+		Trigger.AfterDelay(delay, ProduceTanksGG)
+
+		if #TankAttackGG >= 6 then
+			SendTanksGG()
+			StopTanksGG = true
+		end
+	end)
+end
+
+SendTanksGG = function()
+	Utils.Do(TankAttackGG, function(tnk) tnk.AttackMove(waypoint29.Location) end)
+	Utils.Do(TankAttackGG, function(tnk) tnk.AttackMove(waypoint30.Location) end)
+	Utils.Do(TankAttackGG, function(tnk) tnk.AttackMove(waypoint40.Location) end)
+	Utils.Do(TankAttackGG, function(tnk) tnk.Hunt() end)
+end
---
+++ mods/ra/maps/soviet-05/main.lua
@@ -0,0 +1,185 @@
+IdleHunt = function(unit) if not unit.IsDead and not unit.Type == harv then Trigger.OnIdle(unit, unit.Hunt) end end
+
+CheckForBase = function()
+	baseBuildings = Map.ActorsInBox(Map.TopLeft, waypoint59.CenterPosition, function(actor)
+		return actor.Type == "fact" or actor.Type == "powr"
+	end)
+
+	return #baseBuildings >= 2
+end
+
+CheckForCYard = function()
+	ConYard = Map.ActorsInBox(waypoint78.CenterPosition, waypoint44.CenterPosition, function(actor)
+		return actor.Type == "fact"
+	end)
+
+	return #ConYard >= 1
+end
+
+CheckForSPen = function()
+	SPens = Map.ActorsInBox(Map.TopLeft, waypoint59.CenterPosition, function(actor)
+		return actor.Type == "spen"
+	end)
+
+	return #SPens >=1
+end
+
+RunInitialActivities = function()
+	if Map.Difficulty == "Hard" then 
+		Expansion()
+		ExpansionCheck = true
+		else
+		ExpansionCheck = false
+	end
+	
+	CheckForBase()
+	Actor.Create("camera", true, { Owner = player, Location = waypoint68.Location })
+	
+	if not Harvester.IsDead then
+		Trigger.AfterDelay(1, function(harvester)
+			Harvester.FindResources()
+			Helper.Destroy()
+		end)
+	end
+end
+
+Retreat = function()
+    Reinforcements.Reinforce(player, sov1, sov1Path, 0, function(soldier)
+		soldier.AttackMove(waypoint1.Location)
+	end)
+	Runner1.Move(waypoint53.Location)
+	Runner2.Move(waypoint53.Location)
+	Runner3.Move(waypoint53.Location)
+	ProduceInfantry()
+	Trigger.AfterDelay(DateTime.Minutes(2), ProduceShips)
+	Trigger.Clear(Runner1, "OnDamaged")
+	Trigger.Clear(Runner1, "OnDiscovered")
+    Media.PlaySpeechNotification(player, "ReinforcementsArrived")
+	if not Map.Difficulty == "Easy" then Trigger.AfterDelay(DateTime.Seconds(25), Gguy1) else end
+	Trigger.AfterDelay(DateTime.Minutes(2), Gguy1)
+	Trigger.AfterDelay(DateTime.Minutes(5), Gguy1)
+	Trigger.AfterDelay(DateTime.Minutes(3), function()	reinforceGG = true	end)
+end
+
+Expansion = function()
+	if not ExpansionCheck then
+		mcvtransport.Move(waypoint67.Location)
+		mcvGG.Move(waypoint78.Location)
+		Reinforcements.Reinforce(GoodGuy, {"dd", "dd"}, {waypoint43.Location, waypoint16.Location}, 0, function(ddsquad)
+			ddsquad.AttackMove(waypoint10.Location)
+		end)
+		Trigger.AfterDelay(DateTime.Seconds(3), function(load)
+			mcvGG.IsInWorld = false
+			mcvtransport.LoadPassenger(mcvGG)
+			mcvtransport.Move(waypoint92.Location)
+			Trigger.AfterDelay(DateTime.Seconds(10), function(unload)
+				mcvtransport.UnloadPassengers()
+				Trigger.AfterDelay(DateTime.Seconds(2), function(move)
+					mcvGG.Move(MCVDeploy.Location)
+					Trigger.AfterDelay(DateTime.Seconds(4), function(deploy)
+						mcvGG.Deploy()
+						BuildBase()
+						ExpansionCheck = true
+						mcvtransport.Move(waypoint43.Location)
+						IslandTroops()
+						Trigger.AfterDelay(DateTime.Seconds(7), function(leave)
+							mcvtransport.Destroy()
+						end)
+					end)
+				end)
+			end)
+		end)
+	else
+	end
+end
+
+Tick = function()
+
+	if Greece.HasNoRequiredUnits() and GoodGuy.HasNoRequiredUnits() then
+		player.MarkCompletedObjective(KillAll)
+	end
+	
+	if player.HasNoRequiredUnits() then
+		player.MarkFailedObjective(CaptureObjective)
+		player.MarkFailedObjective(KillAll)
+		GoodGuy.MarkCompletedObjective(BeatUSSR)
+	end
+	
+	-- if DateTime.GameTime % 251 == 0 then Media.DisplayMessage(tostring(GoodGuy.Cash + GoodGuy.Resources)) end
+	
+	if Radar.IsDead == true then
+		player.MarkFailedObjective(CaptureObjective)
+	end
+	
+	if Radar.Owner == player then
+		if not Radstop then
+			if not ExpansionCheck then Expansion() ExpansionCheck = true end
+			player.MarkCompletedObjective(CaptureObjective)
+			Reinforcements.Reinforce(Greece, gguy2, {waypoint2.Location, waypoint3.Location, waypoint68.Location}, 0, function(soldier)
+				soldier.Hunt()
+			end)
+			Radstop = true
+		end
+	end
+	
+	if not baseEstablished and CheckForBase() then
+		baseEstablished = true
+		Para()
+	end
+	
+	if not SPenEstablished and CheckForSPen() then
+		SPenEstablished = true
+		local units = Reinforcements.ReinforceWithTransport(Greece, "lst", arty1reinf, { waypoint44.Location, waypoint7.Location, waypoint9.Location, waypoint26.Location, waypoint18.Location, waypoint54.Location }, { waypoint44.Location })[2]
+		Utils.Do(units, function(unit) IdleHunt(unit) end)
+		if 	not ExpansionCheck then Expansion() ExpansionCheck = true end
+	end
+			
+	if reinforceGG == true then
+		Trigger.AfterDelay(DateTime.Seconds(10), Gguy2)
+		reinforceGG = false
+		if Map.Difficulty == "Easy" then
+			Trigger.AfterDelay(DateTime.Minutes(6), function()	reinforceGG = true	end)
+		elseif Map.Difficulty == "Medium" then
+			Trigger.AfterDelay(DateTime.Minutes(4), function()	reinforceGG = true	end)
+		else Trigger.AfterDelay(DateTime.Minutes(3), function()	reinforceGG = true	end)
+		end
+	end
+end
+
+WorldLoaded = function()
+    player = Player.GetPlayer("USSR")
+	GoodGuy = Player.GetPlayer("GoodGuy") 	
+	Greece = Player.GetPlayer("Greece") 	
+	
+	RunInitialActivities()
+	
+    CaptureObjective = player.AddPrimaryObjective("Capture the Radar")
+    KillAll = player.AddPrimaryObjective("Defeat the Allied Force")
+    BeatUSSR = GoodGuy.AddPrimaryObjective("Defeat the USSR Force")
+	
+	Trigger.OnDamaged(Runner1, Retreat)
+	Trigger.OnDiscovered(Runner1, Retreat)
+	
+	Trigger.OnDamaged(mcvGG, Expansion)
+	Trigger.OnDamaged(mcvtransport, Expansion)
+	
+	Trigger.OnEnteredProximityTrigger(waypoint39.CenterPosition, WRange.New(4 * 1024), function(unit, id)
+		if unit.Owner == player and Radar.Owner == player then
+			Trigger.RemoveProximityTrigger(id)
+			local units = Reinforcements.ReinforceWithTransport(player, "lst", {"mcv", "3tnk", "3tnk", "e1", "e1"}, { waypoint13.Location, waypoint54.Location }, { waypoint13.Location })[2]
+			Utils.Do(units, function(unit) Trigger.OnIdle(unit, function(mcv) mcv.Move( waypoint39.Location) end) end)
+			Para2()
+			ProduceInfantryGG()
+			ProduceTanksGG()
+		end
+	end)
+	
+	Trigger.OnPlayerLost(player, function()
+		Media.PlaySpeechNotification(player, "Lose")
+	end)
+	Trigger.OnPlayerWon(player, function()
+		Media.PlaySpeechNotification(player, "Win")
+	end)
+	
+	Camera.Position = waypoint98.CenterPosition
+end
